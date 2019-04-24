5c7b08fd41eaf5dd6b72ac00eac619ade52938ae
asxi1998 <asxi1998@gmail.com>
2019-04-24 18:51:36 +0700

    hoan

---
+++ DeAnHQTCSDL_qlthuvien/FormMain.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DeAnHQTCSDL_qlthuvien
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+        private void MouseDetect_Tick(object sender, EventArgs e)
+        {
+            if (panelMainMenu.ClientRectangle.Contains(PointToClient(Control.MousePosition)))
+            {
+                panelMainMenu.Width = 267;
+                pictureBox1.Width = 70;
+                pictureBox1.Height = 80;
+                pictureBox1.Location = new Point(95,20);
+            }
+            else
+            {
+                panelMainMenu.Width = 39;
+                pictureBox1.Width = 40;
+                pictureBox1.Height = 50;
+                pictureBox1.Location = new Point(1,32);
+            }
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+       
+        }
+        UserControlQTHT userCtlQTHT = new UserControlQTHT();
+        private void BtnQuanTriHeThong_Click(object sender, EventArgs e)
+        {
+            this.panelExMain.Controls.Clear();
+            this.panelExMain.Controls.Add(userCtlQTHT); 
+            
+
+        }
+        UserControlQLNV userCtlQLNV = new UserControlQLNV();
+        private void BtnQuanLyNghiepVu_Click(object sender, EventArgs e)
+        {
+            this.panelExMain.Controls.Clear();
+            this.panelExMain.Controls.Add(userCtlQLNV);
+        }
+        UserControlDangXuat userCtlDangXuat = new UserControlDangXuat();
+        private void BtnDangXuat_Click(object sender, EventArgs e)
+        {
+            this.panelExMain.Controls.Clear();
+            this.panelExMain.Controls.Add(userCtlDangXuat);
+        }
+        UserControlTroGiup userCtlTroGiup = new UserControlTroGiup();
+        private void BtnTroGiup_Click(object sender, EventArgs e)
+        {
+            this.panelExMain.Controls.Clear();
+            this.panelExMain.Controls.Add(userCtlTroGiup);
+        }
+    }
+}
